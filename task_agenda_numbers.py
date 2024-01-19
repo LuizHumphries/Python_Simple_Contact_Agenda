648bd6e44954c333df10b70c3655b6b84ed81208
Priscila Clarindo <luizgustavo.frota@outlook.com>
2024-01-19 00:22:54 -0300

    last step - missing change favorites if exists

--- task_agenda_numbers.py
+++ task_agenda_numbers.py
@@ -6,9 +6,27 @@ Contacts = List[Contact]
 Favorites = List[Contact]
 
 # Functions
+def _change_default(contacts: Contacts, favorites: Favorites, contact_index: int, variable: str):
+    variable_change_condition = input(f"Do you wanna change the contact {variable}? (Y/N)")
+    if variable_change_condition == "Y" or variable_change_condition == 'y':
+        contacts[contact_index][f"{variable.capitalize()}"] = input(f"New contact {variable}: ")
+    elif variable_change_condition == "N" or variable_change_condition == 'n':
+        contacts[contact_index][f"{variable.capitalize()}"] = contacts[contact_index][f"{variable.capitalize()}"]
+    else:
+        print("The answer must be Y/y or N/n")
+
 def add_contact(contacts: Contacts, contact: Contact) -> None:
     contact = {"Name": contact["Name"], "Phone": contact["Phone"], "Email": contact["Email"], "Favorite": contact["Favorite"]}
     contacts.append(contact)
+
+def see_full_favorite_list(favorites: Favorites) -> None:
+    print("\n*******\nFAVORITE LIST:")
+    for count, contact in enumerate(favorites, start = 1):
+        contact_name = contact["Name"]
+        contact_phone = contact["Phone"]
+        contact_email = contact["Email"]
+        favorite_status = "🌟" if contact["Favorite"] else " "
+        print(f"> {count}. [{favorite_status}] {contact_name} | {contact_phone} | {contact_email}")
 
 def see_full_contact_list(contacts: Contacts) -> None:
     print("\n*******\nCONTACT LIST:")
@@ -18,6 +36,11 @@ def see_full_contact_list(contacts: Contacts) -> None:
         contact_email = contact["Email"]
         favorite_status = "🌟" if contact["Favorite"] else " "
         print(f"> {count}. [{favorite_status}] {contact_name} | {contact_phone} | {contact_email}")
+
+def update_contact(contacts: Contacts, favorites: Favorites, contact_index: int) -> None:
+    _change_default(contacts, favorites, contact_index, "name")
+    _change_default(contacts, favorites, contact_index, "phone")
+    _change_default(contacts, favorites, contact_index, "email")
     
 def add_to_favorites(contacts: Contacts, contact_index: int, favorites: Favorites) -> None:
     if contacts[contact_index] in favorites:
@@ -38,8 +61,6 @@ def remove_from_favorites(contacts: Contacts, contact_index: int, favorites: Fav
 favorites = []
 contacts= []
 
-
-
 while True:
     print("""\nTask Manager Menu:
     1. Add contact
@@ -52,7 +73,6 @@ while True:
 
     contact_list_options = input("Select your option: ")
 
-
     if contact_list_options == "1":
         new_contact_name = input("What is the contact name? ")
         if new_contact_name:
@@ -62,13 +82,24 @@ while True:
             add_contact(contacts, new_contact)
         else:
             print("You cant create a new contact without the Name")
+    elif contact_list_options == "2":
+        see_full_favorite_list(favorites)
     elif contact_list_options == "3":
         see_full_contact_list(contacts)
+    elif contact_list_options == "4":
+        see_full_contact_list(contacts)
+        contact_index_to_update = int(input("Select the contact you want to update: ")) - 1
+        if contact_index_to_update >= 0 and contact_index_to_update < len(contacts):
+            update_contact(contacts, favorites, contact_index_to_update)
+            see_full_contact_list(contacts)
+        else:
+            print("\nContact Invalid") 
+        
     elif contact_list_options == "5":
         see_full_contact_list(contacts)
         contact_index_to_favorite = int(input("Select the contact you want to add to Favorite List: ")) - 1
         if contact_index_to_favorite >= 0 and contact_index_to_favorite < len(contacts):
-            add_to_favorites(contacts, int(contact_index_to_favorite), favorites)
+            add_to_favorites(contacts, contact_index_to_favorite, favorites)
             see_full_contact_list(contacts)
         else:
             print("\nContact Invalid")
@@ -76,7 +107,7 @@ while True:
         see_full_contact_list(contacts)
         contact_index_to_favorite = int(input("Select the contact you want to remove from Favorite List: ")) - 1
         if contact_index_to_favorite >= 0 and contact_index_to_favorite < len(contacts):
-            remove_from_favorites(contacts, int(contact_index_to_favorite), favorites)
+            remove_from_favorites(contacts, contact_index_to_favorite, favorites)
             see_full_contact_list(contacts)
         else:
             print("\nContact Invalid")
